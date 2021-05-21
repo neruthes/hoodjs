afa1509764256e2660a7ac3310bb63078aeef3bb
Neruthes <i@neruthes.xyz>
2021-05-21 18:16:21 +0000

    Regular update 2021-05-19T20:52:07+00:00

--- Hood.js
+++ Hood.js
@@ -191,5 +191,18 @@ Hood.setSrcData = function (fd, dataKey, newValue) {
     Hood._registeredInstances[realFd]._src[dataKey] = newValue;
 };
 document.body.addEventListener('input', Hood.internal.generateRawEventHandler('input'));
-document.body.addEventListener('click', Hood.internal.generateRawEventHandler('click'));
+
+// NOTE: Click events should be handled by each DOM element, which declares a method name for the click event
+document.body.addEventListener('click', function (e) {
+    let rawTarget = e.target;
+    let searchQuery = Hood.internal.searchUpRecursively(rawTarget, ['hood-fd', 'hood-click']);
+    if (searchQuery['hood-fd'] === null || searchQuery['hood-click'] === null) {
+        console.log(`False positive click!`);
+    } else {
+        let fd = searchQuery['hood-fd'];
+        let methodName = searchQuery['hood-click'];
+        Hood.call(parseInt(fd), methodName);
+    };
+});
+
 // NOTE: Focus & Blur cannot be bubbled so they are captured in other ways
